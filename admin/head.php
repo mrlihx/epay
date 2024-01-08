b540dbbf747545bdc7ca6fdc00cc7685bd30c3a9
mrlihx <mrlihx@163.com>
2024-01-08 11:06:02 +0800

    update

--- admin/head.php
+++ admin/head.php
@@ -49,7 +49,7 @@ if ($admin_cdnpublic == 1) {
                     <span class="icon-bar"></span>
                 </button>
                 <a class="navbar-brand" href="./"><?php echo $conf['sitename']; ?>管理中心</a>
-            </div><!-- /.navbar-header -->
+            </div><!-- /.navbar-header  -->
             <div id="navbar" class="collapse navbar-collapse">
                 <ul class="nav navbar-nav navbar-right">
                     <li class="<?php echo checkIfActive('index,') ?>">
